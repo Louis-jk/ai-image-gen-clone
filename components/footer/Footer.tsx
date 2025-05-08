eb59ce4a4e7082228c8feeea70ac67c9fbb940d4
Joonho Kim <lippoint.surf0622@gmail.com>
2025-05-08 14:37:32 +0900

    feat: add Footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-4 border-t border-gray-800">
+        <div className="container mx-auto">
+            <p>Footer</p>
+        </div>
+    </footer>
+  );
+}
