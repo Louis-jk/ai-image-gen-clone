80088090290a1ed9ad3e5e20ae881c156371daed
Joonho Kim <lippoint.surf0622@gmail.com>
2025-05-08 14:07:48 +0900

    chore: initialize project with Next.js and shadcn/ui

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+      <h1 className="text-5xl text-red-950">Home</h1>
+      <Button variant="destructive" size="lg">Shadcn UI</Button>
+    </div>
+  );
+}
