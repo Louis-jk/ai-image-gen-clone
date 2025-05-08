18bf046f7117f098aac065a8091440453595799c
Joonho Kim <lippoint.surf0622@gmail.com>
2025-05-08 14:39:23 +0900

    feat: update home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Home from "@/components/home/Home";
 
-export default function Home() {
+export default function HomePage() {
   return (
    <div>
-      <h1 className="text-5xl text-red-950">Home</h1>
-      <Button variant="destructive" size="lg">Shadcn UI</Button>
+      <Home />
     </div>
   );
 }
